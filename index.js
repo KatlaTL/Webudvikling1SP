8aa26dbeb2e23e013f75bd65e3b9cf8866d04b62
Asger Thorsboe Lundblad <balloupjuske2208@gmail.com>
2023-10-30 11:29:07 +0100

    Updated port to 3306

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 /* const webRoutes = require('./routes/web'); */
 /* const bodyParser = require('body-parser'); */
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3306;
 const app = express();
 
 /* app.use(bodyParser.urlencoded({ extended: false }));
