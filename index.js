b0557502de3aefbe68fbbc216912e07d9de8ebf4
Asger Thorsboe Lundblad <balloupjuske2208@gmail.com>
2023-10-30 12:10:52 +0100

    removed ssl

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const fs = require("fs/promises");
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -7,11 +6,5 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World');
   });
-
-app.get('/.well-known/acme-challenge/kHjQZMGUWXX1nKCjOPI-kRYinufSDPpydmnAaMSSkpg', (req, res) => {    
-    fs.readFile("SSL-certificates.txt")
-        .then(data => res.send(data.toString()));
-});
-
 
 app.listen(port, () => console.log(`server running on port: ${port}`));
