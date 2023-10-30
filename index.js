50727d0f05a7a6ae81d00346caf5116924f87045
AndKrogh <anders.krogh99@gmail.com>
2023-10-30 12:25:43 +0100

    ejs install

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
 
     let {avatarURL, email, id, name} = req.body;
-    
+
   });
 
 app.post ("/", (req, res) => {
