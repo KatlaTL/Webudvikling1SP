19c68d118d2bf1a48df486d7a3d0b76109aa52e8
Asger Thorsboe Lundblad <balloupjuske2208@gmail.com>
2023-10-30 13:10:37 +0100

    added folder structure for ejs

---
+++ routes/web.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+module.exports = () => {
+    //ADD routers here with router.get / router.post
+    
+    return router;
+};
