aa88cca28574a65e73220a65f80fb099814b3d82
AndKrogh <anders.krogh99@gmail.com>
2023-10-30 12:09:04 +0100

    server and post

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 /* const webRoutes = require('./routes/web'); */
 /* const bodyParser = require('body-parser'); */
 
-const port = process.env.PORT || 3306;
+const port = process.env.PORT || 4000;
 const app = express();
 
 /* app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,8 +11,22 @@ app.set('view engine', 'ejs'); */
 
 /* app.use('/', webRoutes()); */
 
+
+
 app.get('/', (req, res) => {
     res.send('Hello World');
   });
+
+app.post ("/", (req, res) => {
+
+    var avatarURL = req.body.avatar;
+    var email = req.body.email;
+    var id = req.body.id;
+    var name = req.body.name;
+    
+    connection.query("INSERT INTO user (avatarURL, email, id, name) VALUES", (avatarURL , email, id, name), function (err, result) {
+        if (err) throw err;
+    });
+});
 
 app.listen(port, () => console.log(`server running on port: ${port}`));
