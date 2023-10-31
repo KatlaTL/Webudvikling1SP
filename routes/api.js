d5af65ef4413bb5e66e9d1b6c8176a4679a8c3f3
Asger Thorsboe Lundblad <balloupjuske2208@gmail.com>
2023-10-31 14:05:30 +0100

    created api shell

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
+const Status = require("../controllers/api/v1/StatusController");
+
 const router = express.Router();
 
 module.exports = () => {
     //ADD routers here with router.get / router.post
+    router.put("/api/vi/status/:id", Status.update);
     return router;
 };
