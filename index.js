9d4bfb9eec73095582cbb14e5c499bd77e737f04
Asger Thorsboe Lundblad <balloupjuske2208@gmail.com>
2023-11-01 15:31:59 +0100

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require('body-parser');
 const webRoutes = require('./routes/web');
 const apiRoutes = require('./routes/api');
@@ -10,6 +11,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+app.use(cors({
+    origin: "https://webdock.io",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: "Content-Type"
+}));
 
 app.use("/", webRoutes());
 app.use("/", apiRoutes());
