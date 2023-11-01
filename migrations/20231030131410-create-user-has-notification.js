5e5d4bb8399162200f28b4ddc4097d592b2540f8
Asger Thorsboe Lundblad <balloupjuske2208@gmail.com>
2023-11-01 12:11:32 +0100

    added migrations models

--- migrations/20231030131410-create-user-has-notification.js
+++ migrations/20231030131410-create-user-has-notification.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('user_has_notifications', {
+    await queryInterface.createTable('User_has_notifications', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -26,6 +26,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('user_has_notifications');
+    await queryInterface.dropTable('User_has_notifications');
   }
 };
--- migrations/20231031083656-create-feature-request.js
+++ migrations/20231031083656-create-feature-request.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('feature_requests', {
+    await queryInterface.createTable('Feature_requests', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -44,6 +44,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('feature_requests');
+    await queryInterface.dropTable('Feature_requests');
   }
 };
