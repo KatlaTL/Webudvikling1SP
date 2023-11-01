cd876ea3a132fa0888aa1a38bd1109d598a28e35
Asger Thorsboe Lundblad <balloupjuske2208@gmail.com>
2023-11-01 13:15:52 +0100

    Updated port to 80

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const webRoutes = require('./routes/web');
 const apiRoutes = require('./routes/api');
-
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 80;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
