bf329f68d9e21b32eea92b8313bcbeb71d41ecce
AndKrogh <anders.krogh99@gmail.com>
2023-11-01 12:29:15 +0100

    insert into db in userscontroller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,9 +1,9 @@
-const { user } = require('../models');
+const { User } = require('../models');
 
 exports.createUsers = async (req, res) => {
   try {
     const { avatarURL, email, id, name } = req.body;
-    const user = await user.create({ avatarURL, email, id, name });
+    const user = await User.create({ avatarURL:avatarURL, email:email, role_id:1, name:name });
     return res.render('login');
   } catch (e) {
     console.log(e);
