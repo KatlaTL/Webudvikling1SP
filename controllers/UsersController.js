d0a2df892c8291f047c28627d8475543e248f13e
AndKrogh <anders.krogh99@gmail.com>
2023-11-01 12:39:46 +0100

    changes to controller and models

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,7 +3,7 @@ const { User } = require('../models');
 exports.createUsers = async (req, res) => {
   try {
     const { avatarURL, email, id, name } = req.body;
-    const user = await User.create({ avatarURL:avatarURL, email:email, role_id:1, name:name });
+    const user = await User.create({ id:id, avatarURL:avatarURL, email:email, role_id:1, name:name });
     return res.render('login');
   } catch (e) {
     console.log(e);
