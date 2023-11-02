7de51cec28d529410914389a730644efc7563f6d
SigneThrane <signecathrane@gmail.com>
2023-11-02 10:31:05 +0100

    besked

--- seeders/20231101112043-demo-feature-request.js
+++ seeders/20231101112043-demo-feature-request.js
@@ -17,28 +17,28 @@ module.exports = {
       category_id: 1
     },
     {
-      id: 1537979745,
+      id: 1537979746,
       title: "Hamsters with tiny hats",
       description: "some description",
       imageURL: null,
       createdAt: new Date(),
       updatedAt: new Date(),
       parent_feature_request_id: null,
-      status_id: 1,
-      user_id: 1,
-      category_id: 1
+      status_id: 2,
+      user_id: 2,
+      category_id: 2
     },
     {
-      id: 1537979745,
+      id: 1537979747,
       title: "Tiny boots on cute tiny fur balls aka hamsters",
       description: "some description",
       imageURL: null,
       createdAt: new Date(),
       updatedAt: new Date(),
       parent_feature_request_id: null,
-      status_id: 1,
-      user_id: 1,
-      category_id: 1
+      status_id: 3,
+      user_id: 3,
+      category_id: 3
     }
   ], {});
 
