b9f4ab043676bdb60206ad15ca5dcf963004e541
AChristineP <94613529+AChristineP@users.noreply.github.com>
2023-11-02 10:35:26 +0100

    update

--- controllers/featureRequestsController.js
+++ controllers/featureRequestsController.js
@@ -4,14 +4,12 @@ const axios = require("axios");
 const express = require('express');
 const app = express();
 
-exports.index = async (req, res) => {
-  try {
-    const featureRequests = await FeatureRequest.findAll();
-    return res.render('pages/featureRequest', {featureRequest});
-  } catch (e) {
-    console.log(e);
-    return res.send('Error');
-  }
+
+const featureRequestData = {
+  userID: "Brugerens ID",
+  title: "Title på feature request",
+  description: "Indholdet i feature requesten",
+  category: "Valgt kategori"
 };
 
 exports.createForm = (req, res) => {
@@ -19,7 +17,20 @@ exports.createForm = (req, res) => {
 };
 
 exports.create = async (req, res) => {
-  try {
+
+  axios.post('https://webdock.io/en/platform_data/feature_requests/new', featureRequestData)
+  .then(response => {
+    if (response.status === 200) {
+      console.log("Feature request added. ID:", response.data.id);
+    } else {
+      console.error("An error occurred:", response.data.message);
+    }
+  })
+  .catch(error => {
+    console.error("An error occurred:", error.message);
+  });
+//axios post her
+    try {
     const { category, title, details } = req.body;
     const featureRequest = await featureRequest.create({category, title, details});
     return res.render('pages/featureRequestCreate');
@@ -27,13 +38,6 @@ exports.create = async (req, res) => {
     console.log(e);
     return res.send('Error');
   }
-};
-
-const featureRequestData = {
-  userID: "Brugerens ID",
-  title: "Title på feature request",
-  description: "Indholdet i feature requesten",
-  category: "Valgt kategori"
 };
 
 axios.post('https://webdock.io/en/platform_data/feature_requests/new', featureRequestData)
