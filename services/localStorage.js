7abcabfbdfe7f28b39a1b61a9be07db911d40ac5
AndKrogh <anders.krogh99@gmail.com>
2023-11-03 12:09:22 +0100

    saving userID to serverside localstorage and storing it in /scratch

--- services/localStorage.js
+++ services/localStorage.js
@@ -1,37 +1,18 @@
 
 exports.loginLocalStorage = (data) => {
     try{
-        console.log(data);
+        console.log(data.id);
 
         if (typeof localStorage === "undefined" || localStorage === null) {
             var LocalStorage = require('node-localstorage').LocalStorage;
             localStorage = new LocalStorage('./scratch');
           } 
           
-          localStorage.setItem('myFirstKey', 'myFirstValue');
-          console.log(localStorage.getItem('myFirstKey'));
+          localStorage.setItem('userID', data.id);
+          console.log(localStorage.getItem('userID'));
     return data;
     } catch(e) {
 
     }
-
-
-    /* console.log(data.User);
-
-    if (typeof localStorage === "undefined" || localStorage === null) {
-        var LocalStorage = require('node-localstorage').LocalStorage;
-        localStorage = new LocalStorage('./scratch');
-    } 
-
-    localStorage.setItem('user', data);
-
-    console.log(localStorage.getItem(data)); */
-
-
-  
-    /* let userArray = data.ssoToken; */
-    /* console.log(userArray); */
-  
-    /* localStorage.setItem('user', JSON.stringify(userArray.name)); */
     
   };
