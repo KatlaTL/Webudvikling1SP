2f797c2decc919c1727020d42d30a77b31b55534
AndKrogh <anders.krogh99@gmail.com>
2023-11-07 14:14:32 +0100

    making comments post and get request

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -4,7 +4,7 @@ exports.comments = async (req, res) => {
   try{
     const Comments = await Comment.findAll();
     console.log(Comments);
-    return res.render('comments', { Comments:Comments});
+    return res.render('comments', { Comments:Comments });
   } catch(e) {
     console.log(e);
   }
@@ -13,7 +13,7 @@ exports.comments = async (req, res) => {
 exports.postComments = async (req,res) => {
   try {
     let comment =req.body.comment;
-    let commentPost = await Comment.create({ comment:comment});
+    let commentPost = await Comment.create({ comment:comment });
     return commentPost;
   } catch (e) {
     console.log(e);
