832dd37654d015fdb6439e6c15f7912438f46239
AndKrogh <anders.krogh99@gmail.com>
2023-11-08 13:40:00 +0100

    comment userid shows user who posted it

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -4,10 +4,12 @@ const { User } = require('../models');
 exports.comments = async (req, res) => {
   try{
     const Comments = await Comment.findAll();
-    console.log(Comments);
+    /* console.log(Comments); */
 
     const Users = await User.findAll();
-    console.log(Users);
+    const User_id = await User.findOne({ where: { id: Number(User.id) }});
+    /* console.log(Users); */
+    console.log(User_id);
 
     return res.render('comments', { Comments:Comments, Users:Users });
   } catch(e) {
