a1fe0efe17ab59628f8a83456e46bbbfbd343778
AChristineP <94613529+AChristineP@users.noreply.github.com>
2023-11-10 09:36:27 +0100

    creating frontend pages

--- controllers/SamplePageController.js
+++ controllers/SamplePageController.js
@@ -3,13 +3,21 @@ const cors = require('cors');
 const app = express();
 app.use(express.static('views'));
 
+app.set('view engine', 'ejs');
+
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 app.use(cors());
 
+const header = {
+    render: (req, res) => {
+        res.render('views/partials/components/header'); 
+    },
+};
+
 const SamplePageController = {
     render: (req, res) => {
-        res.render('pages/SamplePage');
+        res.render('pages/SamplePage'); 
     },
 };
 
