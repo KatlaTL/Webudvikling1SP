5c3cacb05120a0775a177adfbb7d63f4c1ed1bd7
AndKrogh <anders.krogh99@gmail.com>
2023-11-10 12:27:59 +0100

    show user name in comments

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -1,15 +1,41 @@
 const { Comment } = require('../models');
 const { User } = require('../models');
 
+
 exports.comments = async (req, res) => {
   try{
-    const Comments = await Comment.findAll();
-    console.log(Comments);
+    
+    const Comments = await Comment.findAll({
+      /* where: { feature_request_id: 1} */
+    });
 
-    const Users = await User.findAll({
-          where: { id:id }
-      });
-    console.log(Users);
+    let userIds = [];
+    for(let i = 0; i < Comments.length; i++) {
+      userIds.push(Comments[i].user_id);  
+    }
+    
+    /* console.log(userIds);
+ */
+    const Users = await User.findAll();
+    /* console.log(Users.name); */
+
+    let userIdsUsers = [];
+    for(let i = 0; i < Users.length; i++) {
+      userIdsUsers.push(Users[i].id); 
+         
+    }
+
+    /* console.log(userIdsUsers);
+    console.log(userName[1]); */
+
+    let userName = [];
+    for(let i = 0; i < userIds.length; i++) {
+      if( userIds[i] === userIdsUsers[i]){
+        userName.push(Users[i].name);
+      };
+    }
+
+    console.log(userName[1]);
 
     return res.render('comments', { Comments:Comments, Users:Users });
   } catch(e) {
