fe897fa69ef6ec03238f1c7392a93226c3989505
AndKrogh <anders.krogh99@gmail.com>
2023-11-10 15:14:50 +0100

    showing names of users who commented on a specific feature based on userId

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -6,36 +6,42 @@ exports.comments = async (req, res) => {
   try{
     
     const Comments = await Comment.findAll({
-      /* where: { feature_request_id: 1} */
+      where: { feature_request_id: 1}
     });
 
     let userIds = [];
     for(let i = 0; i < Comments.length; i++) {
       userIds.push(Comments[i].user_id);  
+      /* console.log(userIds); */
     }
     
-    /* console.log(userIds);
- */
-    const Users = await User.findAll();
-    /* console.log(Users.name); */
+    /* console.log(userIds); */
+
+    const Users = await User.findAll({
+      where: { id: userIds }
+      });
+    /* console.log(Users); */
 
     let userIdsUsers = [];
     for(let i = 0; i < Users.length; i++) {
       userIdsUsers.push(Users[i].id); 
-         
+     /*  console.log(userIdsUsers); */
     }
 
-    /* console.log(userIdsUsers);
-    console.log(userName[1]); */
+    /* console.log(userIdsUsers); */
 
     let userName = [];
-    for(let i = 0; i < userIds.length; i++) {
-      if( userIds[i] === userIdsUsers[i]){
+    for(let i = 0; i < userIdsUsers.length; i++) {
+      /* userName.push(Users[i].name); */
+
+      if( userIdsUsers[i] == userIds[i]){
         userName.push(Users[i].name);
+        console.log(userName[i]);
       };
     }
 
-    console.log(userName[1]);
+    console.log(userName);
+    console.log(userName.length); 
 
     return res.render('comments', { Comments:Comments, Users:Users });
   } catch(e) {
