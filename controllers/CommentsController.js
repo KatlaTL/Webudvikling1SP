8a069f5b8d52bdd9cc2e2afc8d84f8b3fe2f93b5
AndKrogh <anders.krogh99@gmail.com>
2023-11-16 12:30:16 +0100

    clean up

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -12,27 +12,19 @@ exports.comments = async (req, res) => {
     let userIds = [];
     for(let i = 0; i < Comments.length; i++) {
       userIds.push(Comments[i].user_id);  
-      /* console.log(userIds); */
     }
     
-    /* console.log(userIds); */
-
     const Users = await User.findAll({
       where: { id: userIds }
       });
-    /* console.log(Users); */
 
     let userIdsUsers = [];
     for(let i = 0; i < Users.length; i++) {
       userIdsUsers.push(Users[i].id); 
-     /*  console.log(userIdsUsers); */
     }
-
-    /* console.log(userIdsUsers); */
 
     let userName = [];
     for(let i = 0; i < userIdsUsers.length; i++) {
-      /* userName.push(Users[i].name); */
 
       if( userIdsUsers[i] == userIds[i]){
         userName.push(Users[i].name);
@@ -40,10 +32,7 @@ exports.comments = async (req, res) => {
       };
     }
 
-    console.log(userName);
-    console.log(userName.length); 
-
-    return res.render('comments', { Comments:Comments, Users:Users });
+    return res.render('comments', { Comments:Comments, username:userName });
   } catch(e) {
     console.log(e);
   }
