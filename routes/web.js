ecb73e7f905ff8f140c04b88dd82e051be589bdc
AndKrogh <anders.krogh99@gmail.com>
2023-11-16 13:55:45 +0100

    clean-up

--- routes/web.js
+++ routes/web.js
@@ -7,10 +7,6 @@ const Upvote = require("../controllers/UpvoteController");
 const Login = require("../controllers/LoginController");
 const { userAuth, adminAuth } = require("../middleware/Auth");
 const SamplePageController = require('../controllers/SamplePageController');
-
-const LoginController = require("../controllers/LoginController");
-const LocalStorageController = require("../services/localStorage");
-const UsersController = require("../controllers/UsersController");
 const CommentsController = require('../controllers/CommentsController');
 
 
@@ -39,15 +35,8 @@ module.exports = () => {
     router.get('/login/sso/token', Login.login);
     router.get('/SamplePage', SamplePageController.render);
 
-
-    //ADD routers here with router.get / router.post
-    router.get('/users', UsersController.createUsers);
-    router.get('/index', LoginController.indexRedirect);
-    router.get('/login', LoginController.login);
     router.get('/comments', CommentsController.comments);
     router.post('/postcomments', CommentsController.postComments);
-    router.get('/loggedout', LoginController.localStorageLoggedOutRedirect);
-    router.get('/login/localstorage', LocalStorageController.loginLocalStorage);
 
     return router;
 };
