7ae79c004618e34ec41473e38f1df6cc4d26029c
AChristineP <94613529+AChristineP@users.noreply.github.com>
2023-11-16 21:51:31 +0100

    merge

--- controllers/SamplePageController.js
+++ controllers/SamplePageController.js
@@ -3,31 +3,20 @@ const cors = require('cors');
 const app = express();
 app.use(express.static('views'));
 
-<<<<<<< HEAD
 app.set('view engine', 'ejs');
 
-=======
->>>>>>> signe
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 app.use(cors());
 
-<<<<<<< HEAD
 const header = {
     render: (req, res) => {
-        res.render('views/partials/components/header'); 
+        res.render('views/partials/components/header');
     },
 };
 
-const SamplePageController = {
-    render: (req, res) => {
-        res.render('pages/SamplePage'); 
-=======
 const SamplePageController = {
     render: (req, res) => {
         res.render('pages/SamplePage');
->>>>>>> signe
     },
 };
-
-module.exports = SamplePageController;
--- routes/web.js
+++ routes/web.js
@@ -3,15 +3,9 @@ const express = require("express");
 const Default = require("../controllers/DefaultController")
 const FeatureRequests = require('../controllers/featureRequestsController');
 const Upvote = require("../controllers/UpvoteController");
-<<<<<<< HEAD
 const Login = require("../controllers/LoginController");
-
 const { userAuth, adminAuth } = require("../middleware/auth");
 const SamplePageController = require('../controllers/SamplePageController');
-=======
-const SamplePageController = require('../controllers/SamplePageController');
-const Comment = require('../controllers/CommentController');
->>>>>>> signe
 
 const router = express.Router();
 
@@ -25,13 +19,9 @@ module.exports = () => {
     router.get('/featureRequests/create', FeatureRequests.createForm);
     router.post('/featureRequests/create', FeatureRequests.create);
 
-<<<<<<< HEAD
-    router.get('/emailtest', FeatureRequests.email)
-=======
     router.get('/upvotes/featureRequests/:requestId', Upvote.getUpvotes);
     router.put('/upvotes/featureRequests/:requestId', Upvote.upvote);
     router.get('/requestComment', Comment.index);
->>>>>>> signe
 
     router.get('/featureRequests/:requestId/upvotes', userAuth, Upvote.getUpvotes);
     router.put('/featureRequests/:requestId/upvotes', userAuth, Upvote.upvote);
@@ -41,10 +31,6 @@ module.exports = () => {
 
 
     router.get('/login/ssoredirect', Login.redirect);
-<<<<<<< HEAD
-    router.get('/login/ssotoken', Login.login);
-=======
->>>>>>> signe
     router.get('/SamplePage', SamplePageController.render);
 
     return router;
