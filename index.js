caa5067c699bef6aacac43c96583cc7271be364c
Asger <regsa13@gmail.com>
2023-11-17 10:35:04 +0100

    port

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const bodyParser = require("body-parser");
 const webRoutes = require('./routes/web');
 const apiRoutes = require('./routes/api');
 
-<<<<<<< HEAD
-const port = process.env.PORT || 80;
-=======
 const port = process.env.PORT || 3000;
->>>>>>> development
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
