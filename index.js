ad960bc6ed5f1fe4de2ec4733a118db6a5d642db
Asger <regsa13@gmail.com>
2023-12-18 13:24:04 +0100

    enabled SSL for production

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 require('dotenv').config();
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const webRoutes = require('./routes/web');
 const apiRoutes = require('./routes/api');
 
 const port = process.env.PORT || 80;
+let options = {}
+try {
+    options = {
+        key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/privkey.pem`),
+        cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/fullchain.pem`)
+    }
+} catch (err) {
+    options = null;
+}
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,14 +26,15 @@ app.use(cookieParser());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-
 app.use("/", webRoutes());
 app.use("/api", apiRoutes());
 
 app.get('*', (req, res) => {
     res.status(404).send("404 - Page not found");
-})
+});
 
-app.listen(port, () => console.log(`server running on port: ${port}`));
-
-
+if (options) {
+    https.createServer(options, app).listen(port, () => console.log(`server running on port: ${port}`));
+} else {
+    app.listen(port, () => console.log(`server running on port: ${port}`));
+}
