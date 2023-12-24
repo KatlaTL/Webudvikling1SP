0eecfbd9d0bc09b26a8399730faf87c4a2a9e5f9
Asger Thorsboe Lundblad <balloupjuske2208@gmail.com>
2023-12-24 17:35:17 +0100

    added login to login on comment and create request page

--- public/scripts/loginRedirect.js
+++ public/scripts/loginRedirect.js
@@ -1,14 +1,27 @@
 const redirectUrl = () => {
     const redirectURL = new URL("/internal/login/sso/redirect", document.baseURI);
     const pathName = window.location.pathname.split("/").slice(1).join("/");
-    
+
     redirectURL.searchParams.append("page", pathName); //set the pathname as a query param
-    
-    const queryString = window.location.search; 
+
+    const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
 
     urlParams.forEach((value, key) => {
         redirectURL.searchParams.append(key, value); //attach all existing query params to the new URL
     })
     return redirectURL;
+}
+
+const disabledCTAs = () => {
+    const user = localStorage.getItem("user");
+
+    if (!user) {        
+        const CTAs = document.querySelectorAll(".is-logged-in-cta");
+        
+        for (i in CTAs) {
+            CTAs[i].disabled = true;
+        }
+        
+    }
 }
