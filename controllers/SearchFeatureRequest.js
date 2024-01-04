3b5ed7814684ae0764fdf024d769d0484495ffbc
AndKrogh <anders.krogh99@gmail.com>
2024-01-04 11:05:13 +0100

    changes

--- controllers/SearchFeatureRequest.js
+++ controllers/SearchFeatureRequest.js
@@ -6,7 +6,10 @@ exports.searchFeature = async (req, res) => {
     try {
         let { searchFeature } = req.query;
 
+        console.log(searchFeature);
+
         const search = await Feature_request.findAll({where: { title: {[Op.like]: '%' + searchFeature + '%'} }});
+        console.log(search);
         return res.status(200).json({ searchFeature: search });
    } catch(e) {
     console.log(e);
--- routes/web.js
+++ routes/web.js
@@ -30,7 +30,7 @@ module.exports = () => {
 
     router.post('/comments/postcomments/posted', userAuth, CommentsController.postComments);
 
-    router.get('/search', SearchFeatureRequest.searchFeature);
+    router.get('/search/:searchTerm', SearchFeatureRequest.searchFeature);
     
 
     return router;
